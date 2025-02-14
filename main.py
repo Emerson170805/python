09f35a72774a98af43da4450155a52dff40b822a
Emerson170805 <1555449@senati.pe>
2025-02-14 09:15:56 -0500

    flash

--- main.py
+++ main.py
@@ -3,14 +3,5 @@ numer2=int(input("Introduce el segundo número: "))
 
 suma=numer1+numer2
 resta=numer1-numer2
-division=numer1/numer2
 multiplicacion=numer1*numer2
-potencia=numer1**numer2
-diferencia=numer1%numer2
-
-print("La suma de los números es: ", suma)
-print("La resta de los números es: ", resta)
-print("La división de los números es: ", division)
-print("La multiplicación de los números es: ", multiplicacion)
-print("La potencia de los números es: ", potencia)
-print("La diferencia de los números es: ", diferencia)
+division=numer1/numer2
