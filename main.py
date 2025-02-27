985b856b517221763ffb646b02d0aa26e3fe6d6b
Emerson170805 <1555449@senati.pe>
2025-02-27 14:55:01 -0500

    git commit -m "nuevo"

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ suma=numer1+numer2
 resta=numer1-numer2
 multiplicacion=numer1*numer2
 division=numer1/numer2
+
+
+suma=numer1+numer2
+resta=numer1-numer2
+multiplicacion=numer1*numer2
+division=numer1/numer2
+
+suma=numer1+numer2
+resta=numer1-numer2
+multiplicacion=numer1*numer2
+division=numer1/numer2
